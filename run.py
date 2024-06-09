c649b28b94209c984ebf2bdc31f2de23e311ad3a
AbdAllah-Al-Refai <123490976+AbdAllah-Al-Refai@users.noreply.github.com>
2024-06-09 17:44:29 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,18 +1,10 @@
 from flask import Flask
-from threading import Thread
 
-app = Flask('')
-
+app = Flask('app')
 
 @app.route('/')
 def ping():
-  return "PONG !, HELLO FROM MTC"
+    return "PONG !, HELLO FROM MTC"
 
-
-def run():
-  app.run(host='0.0.0.0', port=8080)
-
-
-def server():
-  t = Thread(target=run)
-  t.start()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
